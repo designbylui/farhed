c77b9a98d2a96b34b340394c0d5deb808df92242
Andre <86534303+designbylui@users.noreply.github.com>
2024-12-01 19:05:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Script básico para animação ou funcionalidades adicionais
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('Obrigado por entrar em contato!');
+});
